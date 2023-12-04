7226a429f886fe58bace32c102c5658ee3a1c2b0
Rohidaszite <150933485+Rohidaszite@users.noreply.github.com>
2023-12-04 10:27:41 +0530

    Add files via upload

---
+++ DigitSum_Prime_Fiboncci.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+class DigitSum_Prime_Fiboncci {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            int num;
+            System.out.println("1) Sum of Digit \n2) Prime Number \n3) Fibonacci Series \n4) Exit");
+            System.out.println("Enter your choice=");
+            int choice = sc.nextInt();
+
+            DigitSum_Prime_Fiboncci dpf = new DigitSum_Prime_Fiboncci();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Number=");
+                    num = sc.nextInt();
+                    dpf.SumOfDigit(num);
+                    break;
+                case 2:
+                    System.out.println("Enter Number=");
+                    num = sc.nextInt();
+                    dpf.Prime(num);
+                    break;
+                case 3:
+                    System.out.println("Enter Number=");
+                    num = sc.nextInt();
+                    dpf.Fibonacci(num);
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Enter valid case only");
+                    break;
+            }
+        }
+    }
+
+    void SumOfDigit(int num) {
+        int sum = 0, lastDigit;
+        while (num > 0) {
+            lastDigit = num % 10;
+            sum += lastDigit;
+            num /= 10;
+        }
+        System.out.println("Sum of Digit=" + sum);
+    }
+
+    void Prime(int num) {
+        boolean flag = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            System.out.println(num + " is Prime");
+        else
+            System.out.println(num + " is Not Prime");
+    }
+
+    void Fibonacci(int num) {
+        int sum = 1;
+        for (int i = 1; i <= num; i++) {
+            sum *= i;
+        }
+        System.out.println("Fibonacci Series of " + num + " is=" + sum);
+    }
+}
